57de2e48d43dbc0eb2f578a652a603b86af84682
RealGungan <69304877+RealGungan@users.noreply.github.com>
2022-09-22 20:55:28 +0200

    Ejercicios 1 y 2

---
+++ PrimerTrimestre/UT02-01/1.php
@@ -0,0 +1,31 @@
+<HTML>
+
+<HEAD>
+    <TITLE> EJ1-Conversion IP Decimal a Binario </TITLE>
+</HEAD>
+
+<BODY>
+    <?php
+    $ip = "192.18.16.204";
+    $number = "";
+    $ip_bin = "";
+
+    for ($i = 0; $i < strlen($ip); $i++) {
+        if ($ip[$i] != ".") {
+            $number .= $ip[$i];
+        } else {
+            $number_bin = decbin($number);
+            $number_bin = str_pad($number_bin, 8, "0", STR_PAD_LEFT);
+            $ip_bin .= $number_bin . ".";
+            $number = "";
+        }
+    }
+
+    $number_bin = decbin($number);
+    $number_bin = str_pad($number_bin, 8, "0", STR_PAD_LEFT);
+    $ip_bin .= $number_bin;
+    printf($ip_bin);
+    ?>
+</BODY>
+
+</HTML>
