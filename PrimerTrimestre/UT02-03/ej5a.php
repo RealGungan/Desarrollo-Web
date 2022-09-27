08b77c5fec6cfa371ded75c1e70412e01a270c1a
RealGungan <69304877+RealGungan@users.noreply.github.com>
2022-09-27 12:15:32 +0200

    s

--- PrimerTrimestre/UT02-03/ej5a.php
+++ PrimerTrimestre/UT02-03/ej5a.php
@@ -10,7 +10,26 @@
     $arr_2 = ["Sistemas Informáticos", "FOL", "Mecanizado"];
     $arr_3 = ["Desarrollo Web ES", "Desarrollo Web EC", "Despliegue", "Desarrollo Interfaces", "Inglés"];
 
+    printf("Array sin función <br>");
+    $arr_sin = [];
+    foreach ($arrays as  $key => $array) {
+        foreach ($array as $finalArray) {
+            $arr_sin[] = $finalArray;
+        }
+    }
+    print_r($arr_sin);
+    printf("<br><br>");
 
+    printf("Array función merge <br>");
+    $arr_merge = array_merge($arr_1, $arr_2, $arr_3);
+    print_r($arr_merge);
+    printf("<br><br>");
+
+    printf("Array función push <br>");
+    $arr_push = array_push($arr_1, $arr_2, $arr_3);
+    print_r($arr_push);
+
+    function array_merge
     ?>
 </BODY>
 
